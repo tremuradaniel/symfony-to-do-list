8c12a392f99ae8c269605b6154499b88a9068e92
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-11 19:15:12 +0300

    Switch status of a task

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -45,7 +45,12 @@ class ToDoListController extends AbstractController
      */
     public function switchStatus($id): Response
     {
-        exit('to do: switch status of the task! ' . $id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $entityManager->persist($task);
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
     }
 
     /**
