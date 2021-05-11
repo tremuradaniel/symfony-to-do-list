153c08bb67e2a06c2a8e4d2e39a2cfa9ca29e702
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-11 17:32:24 +0300

    Create controller methods

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -17,4 +17,28 @@ class ToDoListController extends AbstractController
             'controller_name' => 'ToDoListController'
         ]);
     }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(): Response
+    {
+        exit('to do: create new task');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id): Response
+    {
+        exit('to do: switch status of the task! ' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete($id): Response
+    {
+        exit('to do: delete the task! ' . $id);
+    }
 }
